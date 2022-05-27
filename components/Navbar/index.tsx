11c751497f70565912ea1f022e55ac7136cbaf2b
Robin Zhou <Robin.Zhou@burberry.com>
2022-05-27 13:45:07 +0800

    adjust style of navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import navs from "./config";
 import styles from "./index.module.scss";
 
 const Header: NextPage = () => {
+  const { pathname } = useRouter();
+  console.log(pathname);
+
   return (
     <div className={styles.navbar}>
       <section className={styles.logoArea}>
@@ -12,7 +16,7 @@ const Header: NextPage = () => {
       <section className={styles.linkArea}>
         {navs?.map((nav) => (
           <Link key={nav.label} href={nav.path}>
-            <a className={styles.navItem}>{nav.label}</a>
+            <a className={`${styles.navItem} ${pathname === nav.path ? `${styles.active}`  : ""}`}>{nav.label}</a>
           </Link>
         ))}
       </section>
