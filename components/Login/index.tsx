9f50a5b0c828aaca369e1cad36aa1264ae054abb
Robin Zhou <Robin.Zhou@burberry.com>
2022-05-27 14:39:55 +0800

    Implement fundamental of Login

---
+++ components/Login/index.tsx
@@ -0,0 +1,9 @@
+interface LoginProps {
+  onClose: () => void;
+}
+
+const Login = (props: LoginProps) => {
+  return <div>Login</div>;
+};
+
+export default Login;
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { Button } from "antd";
 import navs from "./config";
+import Login from "components/Login";
 import styles from "./index.module.scss";
 
 const Header: NextPage = () => {
   const { pathname } = useRouter();
-  console.log(pathname);
+  const [isShowLogin, setIsShowLogin] = useState(false);
+
+  const handleWrite = () => {
+    console.log("write");
+  };
+
+  const handleLogin = () => {
+    setIsShowLogin(true);
+  };
+
+  const handleClose = () => {
+    setIsShowLogin(false);
+  };
 
   return (
     <div className={styles.navbar}>
@@ -16,10 +31,32 @@ const Header: NextPage = () => {
       <section className={styles.linkArea}>
         {navs?.map((nav) => (
           <Link key={nav.label} href={nav.path}>
-            <a className={`${styles.navItem} ${pathname === nav.path ? `${styles.active}`  : ""}`}>{nav.label}</a>
+            <a
+              className={`${styles.navItem} ${
+                pathname === nav.path ? `${styles.active}` : ""
+              }`}
+            >
+              {nav.label}
+            </a>
           </Link>
         ))}
       </section>
+      <section className={styles.operationArea}>
+        <Button
+          className={`${styles.opearateButton} ${styles.write}`}
+          onClick={handleWrite}
+        >
+          Write
+        </Button>
+        <Button
+          className={styles.opearateButton}
+          type="primary"
+          onClick={handleLogin}
+        >
+          Login
+        </Button>
+      </section>
+      {isShowLogin ? <Login onClose={handleClose} /> : null}
     </div>
   );
 };
